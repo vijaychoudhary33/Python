1762c86402fc52f99576b49e40d2e09ceffed901
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-11-14 22:52:26 +0530

    sort alphabetically the words form a string

---
+++ Sort string.py
@@ -0,0 +1,11 @@
+my_str = "Hello this Is an Example With cased letters"
+# breakdown the string into a list of words
+words = [word.lower() for word in my_str.split()]
+# sort the list
+words.sort()
+
+# display the sorted words
+
+print("The sorted words are:")
+for word in words:
+   print(word)
