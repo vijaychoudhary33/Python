eaba4d209c96ac54a065e45a262f6e42436db174
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-12-18 23:48:19 +0530

    Add two lists

---
+++ Add list.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Banana"]
+
+# 1. append()
+print(f'Current Fruits List {fruits}')
+
+f = input("Please enter a fruit name:\n")
+fruits.append(f)
+
+print(f'Updated Fruits List {fruits}')
