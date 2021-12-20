9f0d344506d4dd7eff9856f3392b065bb7a35c54
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-12-20 23:25:08 +0530

    Add items in list

---
+++ List add.py
@@ -0,0 +1,6 @@
+fruits = ["Apple","Banana"]
+# 1. append()
+print(f'Current Fruits List {fruits}') 
+f = input("Please enter a fruit name:\n") 
+fruits.append(f)
+print(f'Updated Fruits List {fruits}')
