285343ce23bf88fec33e729248d1d843b4cabccd
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-12-22 21:54:16 +0530

    String

---
+++ Stn.py
@@ -0,0 +1,10 @@
+
+my_string = 'Hello'
+print(my_string)
+my_string = "Hello"
+print(my_string)
+my_string = '''Hello'''
+print(my_string)
+my_string = """Hello, welcome to
+           the world of Python"""
+print(my_string)
