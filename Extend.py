e07a71809a561cfd6c9d7720d2e56e8784c4d85b
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2022-01-01 23:02:53 +0530

    Extend list

---
+++ Extend.py
@@ -0,0 +1,12 @@
+list_num = [] list_num.extend([1, 2])
+# extending list elements
+print
+(list_num) list_num.extend((3, 4))
+# extending tuple elements
+print
+(list_num) list_num.extend(
+"ABC"
+)
+# extending string elements
+print
+(list_num)
