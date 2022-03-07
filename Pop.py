08fb4a0fc30af89e171ce34b0e976c232b2b1478
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2022-03-07 23:32:27 +0530

    List pop

---
+++ Pop.py
@@ -0,0 +1,9 @@
+# create a list of prime numbers
+prime_numbers = [2, 3, 5, 7]
+
+# remove the element at index 2
+removed_element = prime_numbers.pop(2)
+
+print('Removed Element:', removed_element)
+print('Updated List:', prime_numbers)
+
