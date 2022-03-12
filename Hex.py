1bf7360c28e96276978348a90043b79b10321361
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2022-03-12 23:43:09 +0530

    Hex

---
+++ Hex.py
@@ -0,0 +1,11 @@
+number = 435
+print(number, 'in hex =', hex(number))
+
+number = 0
+print(number, 'in hex =', hex(number))
+
+number = -34
+print(number, 'in hex =', hex(number))
+
+returnType = type(hex(number))
+print('Return type from hex() is', returnType)
